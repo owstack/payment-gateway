1d0448964a6e086a14b3077cefb2c9089a7c481f
Ian Patton <ian@owstack.org>
2018-07-25 09:43:00 -0400

    add swagger ui fix and payment email

--- config/default.js
+++ config/default.js
@@ -4,12 +4,14 @@ const environment = process.env.ENVIRONMENT || process.env.NODE_ENV || 'developm
 const certDir = process.env.CERT_DIR || `${__dirname}/../zerossl`;
 
 const externalHostname = process.env.EXTERNAL_HOSTNAME || 'payments.owstack.org';
+const port = Number(process.env.SERVICE_PORT) || 3000;
 
 module.exports = {
+    paymentEmail: process.env.PAYMENT_EMAIL || 'SetPaymentEmailAddressInConfig',
     port: Number(process.env.SERVICE_PORT) || 3000,
     db: process.env.DB_CONN_STRING || `mongodb://localhost:27017/payment-gateway-${environment}`,
     externalHostname,
-    xpub: process.env.XPUB,
+    externalPort: Number(process.env.EXTERNAL_PORT) || port,
     x509: {
         ca: fs.readFileSync(process.env.X509_CA_CERTIFICATE || './zerossl/payments.owstack.org.ca.der'),
         cert: fs.readFileSync(process.env.X509_DER_CERTIFICATE || './zerossl/payments.owstack.org.der'),
@@ -25,6 +27,15 @@ module.exports = {
     https: {
         key: fs.readFileSync(`${certDir}/${externalHostname}.key`),
         cert: fs.readFileSync(`${certDir}/${externalHostname}.crt`)
+    },
+    smtp: {
+        host: process.env.SMTP_HOST || 'smtp.sendgrid.net',
+        port: Number(process.env.SMTP_PORT) || 465,
+        secure: process.env.SMTP_SECURE === 'true',
+        auth: {
+            user: process.env.SMTP_USERNAME || 'apikey',
+            pass: process.env.SMTP_PASSWORD || 'password'
+        }
     },
     explorerAPIs: {
         BTC: process.env.BTC_EXPLORER_API || 'http://btc.livenet.explorer-api.owstack.org/explorer-api',
